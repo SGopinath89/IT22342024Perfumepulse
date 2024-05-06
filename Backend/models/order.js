ad2710ad988122729217d2ec1f740cdf7f3b3970
Chamathka01 <chamathkahettiarachchi@gmail.com>
2024-05-06 17:35:34 +0530

    Created mongo db model for orders.

---
+++ Backend/models/order.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+
+const orderSchema = mongoose.Schema({
+    orderItems: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'OrderItem',
+        required: true
+    }],
+    shippingAddress1: {
+        type: String,
+        rewuired: true
+    },
+    shippingAddress2: {
+        type: String,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    zip: {
+        type: String,
+        required: true,
+    },
+    country: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+        default: 'Pending',
+    },
+    totalPrice: {
+        type: Number,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    dateOrdered: {
+        type: Date,
+        default: Date.now,
+    },
+})
+
+
+
+orderSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+orderSchema.set('toJSON', {
+    virtuals: true,
+});
+
+
+exports.Order = mongoose.model('Order', orderSchema);
