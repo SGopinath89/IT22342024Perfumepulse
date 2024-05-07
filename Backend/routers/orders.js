dec2ac87f959c395ef3765f41ad137f4dcff3c10
Chamathka01 <chamathkahettiarachchi@gmail.com>
2024-05-07 19:50:13 +0530

    Created route for post a product

--- Backend/routers/orders.js
+++ Backend/routers/orders.js
@@ -28,4 +28,47 @@ router.get('/:id', async (req, res)=>{
     res.send(order);
 })
 
+
+router.post('/', async (req, res)=>{
+    const orderItemsIds = Promise.all(req.body.orderItems.map(async (orderItem) =>{
+        let newOrderItem = new OrderItem({
+            quantity: orderItem.quantity,
+            product: orderItem.product
+        })
+
+        newOrderItem = await newOrderItem.save();
+
+        return newOrderItem._id;
+    }))
+    const orderItemsIdsResolved = await orderItemsIds;
+    
+    const totalPrices = await Promise.all(orderItemsIdsResolved.map(async (orderItemId)=>{
+        const orderItem = await OrderItem.findById(orderItemId).populate('product', 'price');
+        const totalPrice = orderItem.product.price * orderItem.quantity;
+        return totalPrice
+    }))
+
+    const totalPrice = totalPrices.reduce((a,b) => a +b, 0);
+
+    let order = new Order({
+        orderItems: orderItemsIdsResolved,
+        shippingAddress1: req.body.shippingAddress1,
+        shippingAddress2: req.body.shippingAddress2,
+        city: req.body.city,
+        zip: req.body.zip,
+        country: req.body.country,
+        phone: req.body.phone,
+        status: req.body.status,
+        totalPrice: totalPrice,
+        user: req.body.user,
+    })
+
+    order = await order.save();
+
+    if(!order){
+        return res.status(404).send('The order cannot be created!')
+    }
+    res.send(order);
+})
+
 module.exports = router;
