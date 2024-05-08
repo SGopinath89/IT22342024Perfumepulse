51cce7d8759612c108fb4927341627aaaef78e52
Chamathka01 <chamathkahettiarachchi@gmail.com>
2024-05-08 22:11:29 +0530

    Created routes for order update and delete.

--- Backend/routers/orders.js
+++ Backend/routers/orders.js
@@ -71,4 +71,37 @@ router.post('/', async (req, res)=>{
     res.send(order);
 })
 
+
+router.put('/:id', async (req, res)=>{
+    const order = await Order.findByIdAndUpdate(
+        req.params.id,
+        {
+            status: req.body.status
+        },
+        {new: true}
+    )
+
+    if(!order){
+        return res.status(404).send('The category cannot be created!')
+    }
+    res.send(order);
+})
+
+
+router.delete('/:id', (req, res)=>{
+    Order.findByIdAndDelete(req.params.id).then(async order =>{
+        if(order){
+            await order.orderItems.map(async orderItem => {
+                await OrderItem.findByIdAndRemove(orderItem)
+            })
+            return res.status(200).json({success: true, message: 'The order is deleted successfully!'})
+        }
+        else{
+            return res.status(404).json({success: false, message: 'order not found!'})
+        }
+    }).catch(err=>{
+        return res.status(500).json({success: false, error: err})
+    })
+})
+
 module.exports = router;
