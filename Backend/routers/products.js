c87685e9ab894d8598230db214858bb72da182ce
LayuruLK <layurulakvidukarunathilaka@gmail.com>
2024-05-09 18:34:18 +0530

    add file upload handeling and create endpoint for post a product

--- Backend/routers/products.js
+++ Backend/routers/products.js
@@ -6,31 +6,31 @@ const mongoose = require('mongoose');
 const multer = require('multer');
 
 
+const FILE_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpeg': 'jpeg',
+    'image/jpg': 'jpg'
+}
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb){
+        const isValid = FILE_TYPE_MAP[file.mimetype];
+        let uploadError = new Error('Invalid image type');
 
+        if(isValid){
+            uploadError = null
+        }
 
+        cb(uploadError, 'public/uploads')
+    },
+    filename: function(req, file, cb) {
+        const fileName = file.originalname.split(' ').join('-');
+        const extension = FILE_TYPE_MAP[file.mimetype];
+        cb(null, `${fileName}-${Date.now()}.${extension}`)
+    }
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const uploadOptions = multer({ storage: storage})
 
 router.get('/allproducts', async (req, res)=>{
     let products =await Product.find({});
@@ -114,5 +114,40 @@ router.get(`/:id`, async(req, res) =>{
     }
     res.send(product);
 }) 
+
+//endpoint for post a new product
+router.post(`/`, uploadOptions.single('image'), async (req, res) =>{
+    
+   
+
+    const file = req.file;
+    if(!file){
+        return res.status(400).send('No image in the request')
+    }
+
+    const fileName = req.file.filename
+    const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
+
+    let product = new Product({
+        name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
+        image: `${basePath}${fileName}`,
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured,
+    })
+
+    product = await product.save();
+
+    if(!product){
+        return res.status(500).send('The product cannot be saved!')
+    }
+        res.send(product);
+})
 
 module.exports = router;
