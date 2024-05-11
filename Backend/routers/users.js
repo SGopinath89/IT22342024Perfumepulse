60975298137c0dc5c98d581f4f636f8eaab66372
Ravindu156 <puresoftlk@gmail.com>
2024-05-11 15:12:44 +0530

    user login and registration

--- Backend/routers/users.js
+++ Backend/routers/users.js
@@ -86,7 +86,59 @@ router.put('/:id',async(req,res)=>{
 
 })
 
+//user login
+router.post('/login',async(req,res)=>{
+    const user=await User.findOne({email:req.body.email})
+    const secret=process.env.secret;
 
+    if(!user){
+        return res.status(400).send('The user not found');
+    }
+
+    if(user && bcrypt.compareSync(req.body.password,user.passwordHash)){
+        const token=jwt.sign(
+            {
+                userId:user.id,
+                isAdmin:user.isAdmin,
+
+            },
+            secret,
+            {expireIn: 'id'}
+        )
+        res.status(200).send({user:user.email,token:token})
+    }else{
+        res.status(400).send('Password is wrong');
+    }
+
+})
+
+
+//user register
+router.post('/register',async(req,res)=>{
+    let user=new User({
+        name:req.body.name,
+        email:req.body.email,
+        passwordHash:bcrypt.hashSync(req.body.password,10),
+        phone:req.body.phone,
+        isAdmin:req.body.isAdmin,
+        street:req.body.street,
+        apartment:req.body.apartment,
+        zip:req.body.zip,
+        city:req.body.city,
+        country:req.body.country,
+    })
+
+    user=await user.save();
+
+    if(!user){
+        return res.status(400).send('the user cannot be created')
+    }
+    
+    
+    res.send(user);
+
+
+})
 
 
 
