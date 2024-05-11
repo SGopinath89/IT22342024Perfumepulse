8e15c7a9fe880f8e55b58ce05b68fb8447bbce4a
Ravindu156 <puresoftlk@gmail.com>
2024-05-11 14:09:18 +0530

    router for updating user details

--- Backend/routers/users.js
+++ Backend/routers/users.js
@@ -54,6 +54,38 @@ router.post('/',async (req,res)=>{
 })
 
 
+//update user details
+router.put('/:id',async(req,res)=>{
+    const userExist=await User.findById(req.params.id);
+    let newPassword
+    if(req.body.password){
+        newPassword=bcrypt.hashSync(req.body.password,10)
+    }else{
+        newPassword=userExist.passwordHash;
+    }
+
+    const user=await User.findByIdAndUpdate(
+        req.params.id,{
+            name:req.body.name,
+            email:req.body.email,
+            passwordHash:newPassword,
+            phone:req.body.phone,
+            isAdmin:req.body.isAdmin,
+            street:req.body.street,
+            apartment:req.body.apartment,
+            zip:req.body.zip,
+            city:req.body.city,
+            country:req.body.country,
+        },
+        {new:true}
+    )
+    if(!user)
+    return res.status(400).send('The user cannot be created')
+
+    res.send(user);
+
+})
+
 
 
 
