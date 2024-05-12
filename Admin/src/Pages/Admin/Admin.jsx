ba30a3f56bc66ab82632186e8bf953703e0e21e9
LayuruLK <layurulakvidukarunathilaka@gmail.com>
2024-05-12 09:48:52 +0530

    Fixed Some Errors

--- Admin/src/Pages/Admin/Admin.jsx
+++ Admin/src/Pages/Admin/Admin.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Admin.css'
 import Sidebar from '../../Components/Sidebar/Sidebar'
 import{ Routes, Route} from 'react-router-dom';
-import AddProduct from '../../Components/AddProduct/AddProduct';
+//import AddProduct from '../../Components/AddProduct/AddProduct';
 import ListProduct from '../../Components/ListProduct/ListProduct';
 
 
@@ -12,7 +12,7 @@ const Admin = () => {
     <div className='admin'>
         <Sidebar/>
         <Routes>
-          <Route path='/addproduct' element={ <AddProduct/> }/>
+          {/* <Route path='/addproduct' element={ <AddProduct/> }/> */}
           <Route path='/listproduct' element={ <ListProduct/> }/>
           
         </Routes>
