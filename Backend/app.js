026c4f34c8cb4c90bc39a3d7126fc1aa559bdea3
LayuruLK <layurulakvidukarunathilaka@gmail.com>
2024-05-16 16:33:33 +0530

    Upadate file structure for Authontication process

--- Backend/app.js
+++ Backend/app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 require('dotenv/config');
+//const authJwt = require('./helpers/jwt');
+//const errorHandler = require('./helpers/error-handler');
 
-
-
+app.use(cors());
+app.options('*', cors());
 
 
 //middleware
@@ -16,7 +17,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
-
+app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
 
 
 
