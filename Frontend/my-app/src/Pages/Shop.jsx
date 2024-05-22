992e07626d9b9232c189d8aee0314f73f5aca005
Ravindu156 <puresoftlk@gmail.com>
2024-05-22 11:50:44 +0530

    created shop page

---
+++ Frontend/my-app/src/Pages/Shop.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+import Sample from '../Components/Sample/Sample';
+import NewCollections from '../Components/NewCollection/NewCollection';
+import Popular from '../Components/Popular/Popular';
+import NewsLetter from '../Components/NewsLetter/NewsLetter';
+
+import axios from 'axios';
+import FindUs from '../Components/FindUs/FindUs';
+import { SearchBar } from '../Components/SearchBar/SearchBar';
+
+const Shop = () => {
+ 
+
+  
+
+  return (
+    <div>
+
+      <Sample />
+      <NewCollections />
+      <Popular />
+      <NewsLetter />
+      <FindUs/>
+      
+    </div>
+  );
+};
+
+export default Shop;
