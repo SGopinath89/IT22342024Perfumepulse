f240b840b3efca6c6f33da01295066983b24e6cc
LayuruLK <layurulakvidukarunathilaka@gmail.com>
2024-05-24 20:20:20 +0530

    Product.jsx Bug fixed

--- Frontend/my-app/src/Components/ProductDisplay/ProductDisplay.jsx
+++ Frontend/my-app/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -5,4 +5,11 @@ import star_icon from '../Assests/star_icon.png';
 import star_dull_icon from '../Assests/star_dull_icon.png';
 import { ShopContext } from '../../Context/ShopContext';
 
+
+const ProductDisplay = (props) =>{
+    return (
+        <div>
+        </div>
+    )
+}
 export default ProductDisplay
