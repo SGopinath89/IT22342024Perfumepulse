55b57dbaf3675f6ac9feffbea8ad4a2679acd0d3
Chamathka01 <chamathkahettiarachchi@gmail.com>
2024-05-24 19:53:58 +0530

    Created ProductDisplay.jsx

---
+++ Frontend/my-app/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,8 @@
+import React, { useContext } from 'react';
+import { useState } from 'react';
+import './ProductDisplay.css'
+import star_icon from '../Assests/star_icon.png';
+import star_dull_icon from '../Assests/star_dull_icon.png';
+import { ShopContext } from '../../Context/ShopContext';
+
+export default ProductDisplay
