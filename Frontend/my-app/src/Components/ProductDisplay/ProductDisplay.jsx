30b233c72edf0a59b6b3029e5460506f9793853f
Chamathka01 <chamathkahettiarachchi@gmail.com>
2024-05-25 09:11:46 +0530

    Set product photos display

--- Frontend/my-app/src/Components/ProductDisplay/ProductDisplay.jsx
+++ Frontend/my-app/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -22,8 +22,71 @@ const ProductDisplay = (props) =>{
     };
     
     return (
-        <div>
+        <div className='productdisplay'>
+         <div className="productdisplay-left">
+            <div className="productdisplay-img-list">
+                <img src={product.images[0]} alt="" />
+                <img src={product.images[1]} alt="" />
+                <img src={product.images[2]} alt="" />
+                <img src={product.images[3]} alt="" />
+            </div>
+            <div className="productdisplay-img">
+                <img className='productdisplay-main-img' src={product.image} alt="" />
+            </div>
+        </div> 
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
         </div>
     )
 }
+
 export default ProductDisplay
