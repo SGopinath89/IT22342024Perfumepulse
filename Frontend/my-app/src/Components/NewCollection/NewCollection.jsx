7a3380663058eae689c7b044641f0ec4296172d0
Ravindu156 <puresoftlk@gmail.com>
2024-05-26 15:13:19 +0530

    created new collections

---
+++ Frontend/my-app/src/Components/NewCollection/NewCollection.jsx
@@ -0,0 +1,36 @@
+import React, {useEffect, useState} from 'react'
+import './NewCollection.css'
+import Item from '../Item/Item'
+
+
+const NewCollections=()=>{
+    const [new_collection,setNewCollection]=useState([])
+
+    useEffect(()=>{
+        fetch('http://local:5000/api/v1/products/newcollections')
+        .then((response)=>response.json())
+        .then((data)=>setNewCollection(data))
+    },[])
+
+    return(
+        <div className='new-collections'>
+            <h1>NEW COLLECTIOND</h1>
+            <hr/>
+
+            <div className="collections">
+                {new_collection.map((item,i)=>{
+                    return <Item key={i} id={item.id} name={item.name} image={item.image} price={item.price} brand={item.brand}/>
+                })}
+            </div>
+
+        </div>
+    )
+
+}
+
+
+export default NewCollections
+
+
+
+
