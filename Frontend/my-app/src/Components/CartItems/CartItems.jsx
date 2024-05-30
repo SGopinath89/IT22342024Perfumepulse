afb1d389257a5788257869dbc531b39497e96a2f
LayuruLK <layurulakvidukarunathilaka@gmail.com>
2024-05-30 09:59:20 +0530

    Started works with CartItem.jsx

---
+++ Frontend/my-app/src/Components/CartItems/CartItems.jsx
@@ -0,0 +1,16 @@
+import React, { useContext, useState, useEffect } from 'react';
+import './CartItems.css';
+import { ShopContext } from '../../Context/ShopContext';
+import remove_icon from '../Assests/cart_cross_icon.png';
+import { Link, useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2'
+
+const CartItems = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CartItems
