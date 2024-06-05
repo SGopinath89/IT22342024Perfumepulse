56e8e6c253cd7b8228d755c070de0a61ef0b75c2
Ravindu156 <puresoftlk@gmail.com>
2024-06-05 23:38:06 +0530

    Checkout page creation

---
+++ Frontend/my-app/src/Pages/Checkout.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './CSS/Checkout.css';
+import OrderForm from '../Components/OrderForm/OrderForm';
+import {useLocation} from 'react-router-dom';
+
+const Checkout=()=>{
+
+    const location=useLocation();
+    const {orderItems,cartItems}=location.state || { orderItems:[], cartItems:[]};
+
+    return(
+        <div className="checkout">
+            <div className="order-form-container">
+                <OrderForm orderItems={OrderForm} cartItems={cartItems} />
+            </div>
+        </div>
+    )
+    
+
+
+
+}
+
+export default Checkout;
+
+
