e8501b4706306d46d21b90a5961122e27632a464
Chamathka01 <chamathkahettiarachchi@gmail.com>
2024-06-05 12:56:12 +0530

    Updated Sample component.

--- Frontend/my-app/src/Components/Sample/Sample.jsx
+++ Frontend/my-app/src/Components/Sample/Sample.jsx
@@ -1,31 +1,32 @@
+import React from 'react'
+import './Sample.css'
 import sample_image from '../Assests/sample_image.png'
 import { SearchBar } from '../SearchBar/SearchBar'
-import './Sample.css'
+import Typical from 'react-typical'
 
-const Sample=()=>{
-    return (
-        <div className='sample'> 
-
-            <div className='sample-left'>
-
-                <div className='sample-moto'>
-                        <p><i>"Uncover Your Fragrance Journey"</i></p>
-                </div>
-            </div>
-            <div className='sample-right'>
-                <img src={sample_image} alt=""/>
-
-            </div>
-            
+const Sample = () => {
+  return (
+    <div className='sample'>
+      
+        <div className="sample-left">
+           
+           <div className="sample-moto">
+           <p style={{ fontSize: '80px'}}>
+              <Typical
+                steps={[
+                  'Uncover Your Fragrance Journey', 1000
+                ]}
+                loop={1}
+                wrapper="span"
+              />
+            </p>
+           </div>
         </div>
-
-)
+        <div className="sample-right">
+            <img src={sample_image} alt="Sample" />
+        </div>
+    </div>
+  )
 }
 
 export default Sample
-
-
-
-
-
-
