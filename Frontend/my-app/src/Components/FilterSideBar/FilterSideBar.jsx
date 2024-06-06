60d7114fec0ada0237c9346775458115299a44e4
Chamathka01 <chamathkahettiarachchi@gmail.com>
2024-06-06 12:07:49 +0530

    Created FilterSideBar.jsx

---
+++ Frontend/my-app/src/Components/FilterSideBar/FilterSideBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './FilterSideBar.css';
+import Category from './Category/Category';
+import Price from './Price/Price';
+import Brand from './Brand/Brand';
+
+const FiltersideBar = ({handleChange}) => {
+  
+  return (
+    <div className='sidebar'>
+      <Category handleChange={handleChange}/>
+      <Price handleChange={handleChange}/>
+      <Brand handleChange={handleChange}/>
+    </div>
+  )
+}
+
+export default FiltersideBar
