7a029fca3a75b415b7bd011c81d3ce8d536d9d4d
Chamathka01 <chamathkahettiarachchi@gmail.com>
2024-06-09 01:18:06 +0530

    Created Products.jsx

---
+++ Frontend/my-app/src/Components/Products/Products.jsx
@@ -0,0 +1,11 @@
+import "./Products.css";
+
+const Products = ({ result }) => {
+    return (
+        <>
+            <section classname="card-container">{result}</section>
+        </>
+    );
+};
+
+export default Products;
