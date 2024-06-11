877c5ea6bd808bfa3e30720d1d784b26b4c7ad1a
Chamathka01 <chamathkahettiarachchi@gmail.com>
2024-06-11 13:33:45 +0530

    Initiating DisplaySearchResult

---
+++ Frontend/my-app/src/Components/DisplaySearchResult/DisplaySearchResult.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Item from '../Item/Item'; // Import your Item component
+
+const DisplaySearchResult = () => {
+    
+}
+
+export default DisplaySearchResult;
