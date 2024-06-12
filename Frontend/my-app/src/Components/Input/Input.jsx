007de68f08f7f020c29669be042635624604c57b
Chamathka01 <chamathkahettiarachchi@gmail.com>
2024-06-12 19:57:42 +0530

    Created Input.jsx

---
+++ Frontend/my-app/src/Components/Input/Input.jsx
@@ -0,0 +1,11 @@
+const Input = ({ handleChange, value, title, name}) => {
+    return (
+        <label className="sidebar-label-container">
+            <input onChange={handleChange} type="checkbox" value={value} name={name} />
+            <span className="checkmark"></span>
+            {title}
+        </label>    
+     );
+};
+
+export default Input;
