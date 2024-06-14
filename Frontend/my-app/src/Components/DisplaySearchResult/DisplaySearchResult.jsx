15a731d5fa02f63c20e0b9de050971d5deb46389
Chamathka01 <chamathkahettiarachchi@gmail.com>
2024-06-14 20:00:18 +0530

    Finished DisplaySearchResult.jsx

--- Frontend/my-app/src/Components/DisplaySearchResult/DisplaySearchResult.jsx
+++ Frontend/my-app/src/Components/DisplaySearchResult/DisplaySearchResult.jsx
@@ -3,7 +3,42 @@ import axios from 'axios';
 import Item from '../Item/Item'; // Import your Item component
 
 const DisplaySearchResult = () => {
-    
-}
+    const [searchQuery, setSearchQuery] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+
+    useEffect(() => {
+        const fetchSearchResults = async () => {
+            try{
+                // Send a GET request to your backend API with the search query
+                const response = await axios.get(`http://localhost:5000/api/v1/products/search?q=${searchQuery}`);
+                setSearchResults(response.data); // Update search results state with the fetched data
+            } catch (error) {
+                console.error('Error fetching search results:', error);
+            }
+        };
+
+        if (searchQuery !== '') {
+            fetchSearchResults();
+        } else {
+            setSearchResults([]); // Clear search results if search query is empty
+        }
+    }, [searchQuery]); // Trigger effect when searchQuery changes
+
+    return (
+        <div>
+            {/* Render search results if searchResult is defined */}
+            {searchResults && searchResults.map((product) => (
+                <Item
+                key={product.id}
+                id={product.id}
+                image={product.image}
+                name={product.name}
+                brand={product.brand}
+                price={product.price}
+                />
+            ))}
+        </div>
+    );
+};
 
 export default DisplaySearchResult;
