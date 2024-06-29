007d1c4c9f81ad1c0ddd7b00a7b1c545c387a2b7
Ravindu156 <puresoftlk@gmail.com>
2024-06-29 20:49:56 +0530

    State management for Form Data

--- Frontend/my-app/src/Pages/LoginSignup.jsx
+++ Frontend/my-app/src/Pages/LoginSignup.jsx
@@ -1,7 +1,25 @@
 import React, { useState } from 'react';
 import './CSS/LoginSignup.css';
 
+export const LoginSignup = () => {
+  const [state, setState] = useState("Login");
+  const [formData, setFormData] = useState({
+    name: "",
+    phone: "",
+    street: "",
+    apartment: "",
+    zip: "",
+    city: "",
+    country: "",
+    password: "",
+    email: "",
+    profilePhoto: null
+  });
 
+  const [loginformData, setLoginFormData] = useState({
+    password: "",
+    email: ""
+  });
 
 
 
