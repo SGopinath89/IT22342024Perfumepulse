2b2479374cb26f9547763abb597666c0e7f9d318
Chamathka01 <chamathkahettiarachchi@gmail.com>
2024-07-01 10:26:01 +0530

    Fix errors

--- Admin/src/main.jsx
+++ Admin/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
+import Sidebar from './Components/Sidebar/Sidebar.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
